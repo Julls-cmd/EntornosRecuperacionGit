eaf6d7bce3142f8c2a0213c2e2b4f8289ad55f59
Julls-cmd <julioleivafranco@gmail.com>
2025-06-19 10:36:49 +0200

    version 1.2_A_JFL

--- prueba_escrita_a_JFL/src/prueba_escrita_a_JFL/Menu.java
+++ prueba_escrita_a_JFL/src/prueba_escrita_a_JFL/Menu.java
@@ -6,6 +6,7 @@ public class Menu {
 		// TODO Auto-generated method stub
 		System.out.println("version 1.0_A");
 		System.out.println("versión 1.1_A_JFL");
+		System.out.println("version 1.2_A_JFL");
 
 	}
 
